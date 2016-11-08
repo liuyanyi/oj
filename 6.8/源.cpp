3a1e9d0e09c326a296bab8245f66790c41cc90bc
liuyanyi <wolfsonliu@163.com>
2016-11-08 16:54:22 +0800

    push

--- 6.8/源.cpp
+++ 6.8/源.cpp
@@ -2,12 +2,16 @@
 #include<stdlib.h>
 int main()
 {
-	float a = 2, b = 3,z,m;
-	while ()
+	float a = 1, b = 2,c=3,s,i,n;
+	scanf("%f",&n);
+	for (i=1;i<=n;i++)
 	{
+		s+=b/a;
 		c = a + b;
 		a = b;
 		b = c;
-
 	}
+	printf("%.2f",s);
+	system("pause");
+	return 0;
 }
