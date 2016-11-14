85bf74a90c0ae4ab220210a27bf7e68de7d779cf
liuyanyi <wolfsonliu@163.com>
2016-11-14 21:20:35 +0800

    6.7fin

--- 6.7/源.cpp
+++ 6.7/源.cpp
@@ -1,38 +1,25 @@
-#include<stdio.h> 
-#include<stdlib.h> 
+#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	int i, j, yin,num,a;
-	scanf_s("%d",&num);
-	for (i = 1; i<num; i++)
+	int m,s,i,l;
+	scanf_s("%d", &l);
+	for (m = 2; m<l; m++)
 	{
-		int s = 0;
-		yin = i;
-		for (j = 1; j<yin; j++)
-		{
-			if (yin%j == 0)
-				s = s + j;
-		}
-		if (yin == s)
-		{
-			printf("%d its factors are ", yin);
-			a = yin;
-			while (a != 1)
+		s = 0;
+		for (i = 1; i<m; i++)
+			if (m%i== 0)
+				s += i;
+			if (s == m)
 			{
-				printf("1 2");
-				if (a % 2 == 0)
+				printf("%d its factors are ", m);
+				for (i = 1; i<m; i++)
 				{
-					a = a / 2;
-					printf("%d ", a);
+					if (m%i == 0)
+						printf("%d ", i);
 				}
-				else if (a % 2 == 1)
-				{
-					a = (a + 1) / 2;
-					printf("%d ", (a + 1) / 2);
-				}	
+				printf("\n");
 			}
-			printf("\n");
-		}
 	}
 	system("pause");
 	return 0;
